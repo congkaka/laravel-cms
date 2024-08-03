2c814c257447975d798a659489205cada2284bbc
Congkaka <trinhvancong1996@gmail.com>
2024-08-04 01:10:06 +0700

    update avatar user login

--- resources/views/admin/product/detail.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('admin.layouts.app')
-@section('content')
-    detail product
-@endsection
-@push('custom-scripts')
-
-@endpush
--- resources/views/admin/product_variant/detail.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('admin.layouts.app')
-@section('content')
-    detail Category
-@endsection
-@push('custom-scripts')
-
-@endpush
